203eb08a9b3da0b7a032f5bf77be24711cbd7d01
Dawson Claybrook <joystickclay@gmail.com>
2023-11-19 19:07:32 -0700

    Added basic typeDefs types for the models. Added extra attributes to the models

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('mongoose');
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  postId: { type: mongoose.Schema.Types.ObjectId, ref:'Post', required: true },
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
