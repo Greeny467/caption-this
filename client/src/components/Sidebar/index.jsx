9a9db6328b079bc2fa40f1318f831e1eb9d8fe58
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-20 21:32:11 -0700

    Converted Diego's elements into react

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -1,10 +1,52 @@
+import './Sidebar.css';
 
 export default function Sidebar() {
-
-
   return (
     <>
-
+      <div className="sidebar">
+        <ul>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-fire"></i>
+              </span>
+              <span className="title">Trending</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-fire-extinguisher"></i>
+              </span>
+              <span className="title">Roast Me</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-coffee"></i>
+              </span>
+              <span className="title">Deep Captions</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="active">
+              <span className="icon">
+                <i className="fas fa-video"></i>
+              </span>
+              <span className="title">Videos</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-dumpster-fire"></i>
+              </span>
+              <span className="title">Random</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
--- client/src/pages/Feed/index.jsx
+++ client/src/pages/Feed/index.jsx
@@ -1,7 +1,14 @@
+import './Feed.css'
+
 export default function Feed() {
   return (
-    <>
-      <h1>Feed</h1>
-    </>
+        <div className="main_container">
+          <div className="post">
+            I SHOULD ADD UPVOTES FIRST AND THAN FIGURE OUT THE POST MAYBEE IDK
+          </div>
+          <div className="post">ITS 4AM, IM TOO BORED TO BE DOING THIS</div>
+          <div className="post">THIS IS WHERE THE POSTS WILL BE RENDERED</div>
+          <div className="post">STILL WORKING ON THE STRUCTURE</div>
+        </div>
   );
 }
