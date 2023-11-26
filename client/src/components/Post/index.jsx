a697e23bc7c14ff0f3a31a5805a561d007857367
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-25 17:58:49 -0700

    Card enhancements

---
+++ client/src/components/Post/index.jsx
@@ -0,0 +1,19 @@
+import "./Post.css";
+
+export default function Post({ children, img, href, ...props }) {
+  if (img) {
+    return (
+      <a href={href} className="post" {...props}>
+        <img src={img} alt="post_img" />
+        <span className="postText">{`"${children}"`}</span>
+      </a>
+    );
+  }
+
+  return (
+    <a href={href} className="post" {...props}>
+      <img src={"https://placehold.co/800x400?text=Post"} alt="post_img" />
+      <span className="postText">{`"${children}"`}</span>
+    </a>
+  );
+}
