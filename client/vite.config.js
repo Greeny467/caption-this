929dbb92e4f1d13fee379ac15d3aeb0cf56c7100
Nathan Greeff <nathangreeff@hotmail.com>
2023-12-03 00:17:56 -0700

    deployment fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: process.env.URI || 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
