e605cce6826d4d95f0f9ed4a03ba424ce3e4ad55
Dawson Claybrook <joystickclay@gmail.com>
2023-12-04 03:47:21 -0700

    changing the postPage up a tad

--- client/src/pages/postPage/postPage.jsx
+++ client/src/pages/postPage/postPage.jsx
@@ -6,117 +6,112 @@ import { useQuery } from '@apollo/client';
 
 import CommentSection from '../../components/commentSection';
 
-export default function PostPage () {
-    const {postId} = useParams();
-    console.log(postId);
+export default function PostPage() {
+  const { postId } = useParams();
+  console.log(postId);
 
-    const [post, setPost] = useState(undefined);
-    const [pageType, setPageType] = useState('post');
+  const [post, setPost] = useState(undefined);
+  const [pageType, setPageType] = useState('post');
 
-    const { loading, error, data } = useQuery(SINGLE_POST, {
-        variables: {
-          requestedPostId: postId
-        },
-    });
+  const { loading, error, data } = useQuery(SINGLE_POST, {
+    variables: {
+      requestedPostId: postId,
+    },
+  });
 
-    useEffect(() => {
-        if (!loading && !error && data) {
-            console.log(data.singlePost)
-            setPost(data.singlePost);
-        }
-    }, [loading, error, data]);
-
-
-    const handlePageTypeChange = () => {
-        if(pageType === 'post') {
-            setPageType('vote');
-        }
-        else{
-            setPageType('post');
-        };
-    };
-
-
-    if(pageType === 'post') {
-        return(
-            <>
-                {post !== undefined ? (
-                    <>
-                        <a id='backbutton' href='/'>Back</a>
-                        <input type='checkbox' id='pageTypeToggle' checked={pageType === 'vote'} onClick={handlePageTypeChange}>pageType</input>
-                        <div>
-                            <section>
-                                <a href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
-                                <h3>Other info?</h3>
-                            </section>
-                            <section>
-                                <img src={post.imageURL}/>
-                                <div>
-                                    {post.caption !== null ? ( 
-                                        <>
-                                            <a href={`/dashboard/${post.caption.user._id}`}>{post.caption.user.username}</a>
-                                            <h4>{post.caption.text}</h4>
-                                        </>
-                                        ):(
-                                            <p>Caption hasn't been chosesn yet</p>
-                                        )
-                                    }
-                                </div>
-                            </section>
-                            
-        
-                            <CommentSection post={post} />
-                        </div>
-                    </>
-                ):(
-                    <h1>Post Not Loaded</h1>
-                )}
-            
-            
-            
-            </>
-        );
-    };
-
-    if(pageType === 'vote') {
-        return(
-            <>
-                {post !== undefined ? (
-                    <>
-                        <div>
-                            <a href='/' id='backButton'>Back</a>
-                            <input type='checkbox' id='pageTypeToggle' checked={pageType === 'vote'} onChange={handlePageTypeChange}>pageType</input>
-                            <section>
-                                <a href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
-                                <h3>Other info?</h3>
-                            </section>
-                            <section>
-                            <img src={post.imageURL}/>
-                            {post.caption !== null ?
-                                (
-                                    <div>
-                                        <a href={`/dashboard/${post.caption.user._id}`}>{post.caption.user.username}</a>
-                                        <h4>{post.caption.text}</h4>
-                                    </div>
-                                ) : (
-                                    <div>
-                                        <p>Caption hasn't been chosen yet</p>
-                                    </div>
-                                )
-                            }
-                            </section>
-                            <section>
-                                <h1>leaderboard component here</h1>
-                            </section>
-                        
-                        </div>
-                    </>
-                ):(
-                    <h1>Post not loaded</h1>
-                )}
-            </>
-        )
+  useEffect(() => {
+    if (!loading && !error && data) {
+      console.log(data.singlePost);
+      setPost(data.singlePost);
     }
+  }, [loading, error, data]);
 
-    
+  const handlePageTypeChange = () => {
+    if (pageType === 'post') {
+      setPageType('vote');
+    } else {
+      setPageType('post');
+    }
+  };
+
+  if (loading) {
+    return <p>Loading...</p>; // You can replace this with a loading spinner or any other loading indicator
+  }
+
+  if (error || !post) {
+    return <h1>Error loading post</h1>;
+  }
+
+  return (
+    <>
+      <a id='backbutton' href='/'>
+        Back
+      </a>
+      <input
+        type='checkbox'
+        id='pageTypeToggle'
+        checked={pageType === 'vote'}
+        onChange={handlePageTypeChange}
+      />
+      {pageType === 'post' && (
+        <div>
+          <section>
+            <a href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
+            <h3>Other info?</h3>
+          </section>
+          <section>
+            <img src={post.imageURL} />
+            <div>
+              {post.caption !== null ? (
+                <>
+                  <a href={`/dashboard/${post.caption.user._id}`}>
+                    {post.caption.user.username}
+                  </a>
+                  <h4>{post.caption.text}</h4>
+                </>
+              ) : (
+                <p>Caption hasn't been chosen yet</p>
+              )}
+            </div>
+          </section>
+          <CommentSection post={post} />
+        </div>
+      )}
+      {pageType === 'vote' && (
+        <div>
+          <a href='/' id='backButton'>
+            Back
+          </a>
+          <input
+            type='checkbox'
+            id='pageTypeToggle'
+            checked={pageType === 'vote'}
+            onChange={handlePageTypeChange}
+          />
+          <section>
+            <a href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
+            <h3>Other info?</h3>
+          </section>
+          <section>
+            <img src={post.imageURL} />
+            {post.caption !== null ? (
+              <div>
+                <a href={`/dashboard/${post.caption.user._id}`}>
+                  {post.caption.user.username}
+                </a>
+                <h4>{post.caption.text}</h4>
+              </div>
+            ) : (
+              <div>
+                <p>Caption hasn't been chosen yet</p>
+              </div>
+            )}
+          </section>
+          <section>
+            <h1>leaderboard component here</h1>
+          </section>
+        </div>
+      )}
+    </>
+  );
 }
