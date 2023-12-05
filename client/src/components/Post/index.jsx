12bedaa99a5046ab7d5214163e648034c3910d2c
Nathan Greeff <nathangreeff@hotmail.com>
2023-12-04 18:36:32 -0700

    styling

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -5,7 +5,7 @@ export default function Post({ post }) {
   console.log(post)
   return (
     <a href={`/posts/${post._id}`} className="post">
-      <a href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
+      <a className="userName" href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
       <img src={post.imageURL ? post.imageURL : "https://placehold.co/800x600?text=Missing Image"} alt="post_img" />
       <h2>{post.caption ? post.caption : ''}</h2>
     </a>
