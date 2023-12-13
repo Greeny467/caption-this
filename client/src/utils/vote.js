a65c4872b23aca4aab5f4d35ca46086cf37a35a2
Dawson Claybrook <joystickclay@gmail.com>
2023-12-13 11:45:24 -0700

    removed unnessesary use of singleCaption query

--- client/src/utils/vote.js
+++ client/src/utils/vote.js
@@ -108,14 +108,6 @@ export default async function vote (user, caption) {
         if(!removeUserVoteData) {
             throw new Error('failed to remove vote from user');
         };
-
-        const updatedCaption = await findCaption(captionId);
-
-        if(!updatedCaption) {
-            throw new Error('failed to find downvoted caption');
-        };
-
-        return updatedCaption;
     }
 
     if(hasVote){
@@ -146,14 +138,6 @@ export default async function vote (user, caption) {
             throw new Error('failed to increase caption vote');
         }
 
-
-        const newCaption = await findCaption(captionId);
-
-        if(!newCaption) {
-            throw new Error('failed to find new caption');
-        };
-
-        return newCaption;
     };
 
     if(!hasVote){
@@ -169,13 +153,6 @@ export default async function vote (user, caption) {
             throw new Error('failed to increase caption vote for initial vote.');
         };
 
-        const newCaption = await findCaption(captionId);
-
-        if(!newCaption) {
-            throw new Error('failed to find updated caption');
-        };
-
-        return newCaption;
     }
 };
 
