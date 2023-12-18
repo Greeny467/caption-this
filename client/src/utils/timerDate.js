c21c2ac23ba4b950f72bf29881b76477da118957
Dawson Claybrook <joystickclay@gmail.com>
2023-12-18 15:36:06 -0700

    fixing timerDate.js export so its properly defined

--- client/src/utils/timerDate.js
+++ client/src/utils/timerDate.js
@@ -1,9 +1,7 @@
-
-
-export default setTimerDate = (timer) => {
+const setTimerDate = (timer) => {
     const currentDate = new Date();
-
     const futureDate = new Date(currentDate.getTime() + timer * 60000);
     return futureDate;
 };
 
+export default setTimerDate;
