72294a3c0a9eba411c58ae77b4402a1792ffea2f
Dawson Claybrook <joystickclay@gmail.com>
2023-12-20 12:58:13 -0700

    fixed issue with post component using caption object as a component of some sort

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -7,7 +7,12 @@ export default function Post({ post }) {
     <a href={`/posts/${post._id}`} className="post">
       <a className="userName" href={`/dashboard/${post.user._id}`}>{post.user.username}</a>
       <img src={post.imageURL ? post.imageURL : "https://placehold.co/800x600?text=Missing Image"} alt="post_img" />
-      <h2>{post.caption ? post.caption : ''}</h2>
+      <div>{post.caption ? (
+        <>
+          <h2>{post.caption.user.username}</h2>
+          <h1>{post.caption.text}</h1>
+        </>
+      ) : ''}</div>
     </a>
   );
 }
